ccd7410445083ef6738d0917c3a523a0d472485e
satsol01 <satsol740@gmail.com>
2023-06-30 09:46:12 +0700

    Update Style in View  Donor Chart and Donor List (30/06/2023)

--- src/pages/donorList/cardDonorList.js
+++ src/pages/donorList/cardDonorList.js
@@ -4,10 +4,12 @@ import { Input } from "antd";
 import { Select } from "antd";
 import "./donorList.css";
 import { Pagination } from "antd";
-import FilterForm from "../../components/filterForm";
+import { useNavigate } from "react-router-dom";
 const handleChange = (value) => {
+  console.log(value);
 };
-const CardDonorList = () => {
+const CarddDonorList = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <div className="row">
@@ -66,7 +68,35 @@ const CardDonorList = () => {
           />
         </div>
       </div>
-      <FilterForm />
+      <table className="mt-4">
+        <thead className="Header_List">
+          <tr>
+            <th>Date</th>
+            <th>Order ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quanlity</th>
+            <th>Total</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="border-botton h5-bold-size13">
+            <td>2017-09-29 </td>
+            <td>200398</td>
+            <td>iPhone X 64Gb </td>
+            <td>$999.00</td>
+            <td>1eeee</td>
+            <td>$999.00</td>
+            <td>
+              {" "}
+              <a href="#" onClick={() => navigate("/cardDonor")}>
+                View
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
       <div className="PaginationFrom d-flex justify-content-end mt-3">
         <Pagination
           total={20}
@@ -78,4 +108,4 @@ const CardDonorList = () => {
     </div>
   );
 };
-export default CardDonorList;
+export default CarddDonorList;
